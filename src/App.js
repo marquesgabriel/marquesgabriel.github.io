bc7403eb23cf7b937179504b292bdc28017dc733
marquesgabriel <gabrielf.pmarques@gmail.com>
2019-07-21 12:02:54 -0300

    Add basic background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+
+{/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+        </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+        </a>
+</header> */}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-bg-gradient">
+      <section class="moving-grid">
+        <div class="container">
+          <div class="static-lines">
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+            <div class="vert"></div>
+          </div>
+          <div class="moving-lines">
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+            <div class="horz"></div>
+          </div>
+        </div>
+      </section>
+      <div class="content-cont">
+        {/* <div class="text-body">
+          <h1>
+            <span>Your Name</span>
+            <h3>right here</h3>
+          </h1>
+        </div> */}
+      </div>
     </div>
   );
 }
